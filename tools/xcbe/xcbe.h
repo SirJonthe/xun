d2cca106e193e770df2e366dec08f7847203c368
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2024-03-11 16:32:42 +0100

    Error info.

--- tools/xcbe/xcbe.h
+++ tools/xcbe/xcbe.h
@@ -12,13 +12,27 @@ struct xbinary
 	U16    size;
 };
 
+struct xc_error
+{
+	enum {
+		NONE,
+		MEMORY,
+		REDEF,
+		VERIFY,
+		INTERNAL
+	};
+	token tok;  // The token generating the error.
+	U16   code; // The error code.
+};
+
 /// @brief Contains metadata about an output XASM binary.
 struct xc_out
 {
-	lexer   l;           // The state of the lexer when successfully exiting the assembler.
-	xbinary binary;      // The state of the output binary when successfully existing the assembler.
-	token   max;         // The highest reached token in the input token stream. Generally only interesting if the assembly failed.
-	U16     errors;      // The number of confirmed errors encountered.
+	lexer    l;      // The state of the lexer when successfully exiting the assembler.
+	xbinary  binary; // The state of the output binary when successfully existing the assembler.
+	token    max;    // The highest reached token in the input token stream. Generally only interesting if the assembly failed.
+	U16      errors; // The number of confirmed errors encountered.
+	xc_error error;  //
 };
 
 #endif
